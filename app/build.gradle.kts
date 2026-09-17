58c8ed441bb70d91762dc67894c52dd7d613be6f
Diego <egoroof1@gmail.com>
2026-09-17 22:11:36 +0300

    ci/cd check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.diego.shoping_list"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.diego.shoping_list"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
