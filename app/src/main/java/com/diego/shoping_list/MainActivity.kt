ddb8f00a011854f3f8470d637b16210529f74554
Diego <egoroof1@gmail.com>
2026-09-20 21:51:56 +0300

    добавил тест на продукты

--- app/src/main/java/com/diego/shoping_list/MainActivity.kt
+++ app/src/main/java/com/diego/shoping_list/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.diego.shoping_list.compose.TestDbProductScreen
 import com.diego.shoping_list.compose.TestDbScreen
 import com.diego.shoping_list.ui.theme.Shoping_listTheme
 
@@ -18,7 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Shoping_listTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TestDbScreen(modifier = Modifier.padding(innerPadding))
+//                    TestDbScreen(modifier = Modifier.padding(innerPadding))
+                    TestDbProductScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
