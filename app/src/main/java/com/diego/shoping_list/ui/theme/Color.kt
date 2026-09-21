cd87b90f223b12fc8d0730706a44b638adbb5ecc
P@Cm4n <verigindanya@yandex.ru>
2026-09-21 19:55:12 +0300

    Added colors for theme and switch hir

--- app/src/main/java/com/diego/shoping_list/ui/theme/Color.kt
+++ app/src/main/java/com/diego/shoping_list/ui/theme/Color.kt
@@ -2,10 +2,52 @@ package com.diego.shoping_list.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Светлая тема
+val primaryLight = Color(0xFF845416)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFFFDCBB)
+val onPrimaryContainerLight = Color(0xFF2B1700)
+
+val secondaryContainerLight = Color(0xFFFEDDBD)
+val onSecondaryContainerLight = Color(0xFF281805)
+
+val surfaceLight = Color(0xFFFFF8F4)
+val onSurfaceLight = Color(0xFF211A14)
+val onSurfaceVariantLight = Color(0xFF50453A)
+
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFFFF1E7)
+val surfaceContainerHighLight = Color(0xFFF4E6DA)
+val surfaceContainerHighestLight = Color(0xFFEEE0D5)
+
+val outlineLight = Color(0xFF827568)
+val outlineVariantLight = Color(0xFFD4C4B5)
+
+val inversePrimaryLight = Color(0xFFFABA73)
+val scrimLight = Color(0xFF000000)
+
+
+// Темная тема
+val primaryDark = Color(0xFFFFBE77)
+val onPrimaryDark = Color(0xFF241200)
+val primaryContainerDark = Color(0xFFBE8543)
+val onPrimaryContainerDark = Color(0xFF000000)
+
+val secondaryContainerDark = Color(0xFFA78C70)
+val onSecondaryContainerDark = Color(0xFF000000)
+
+val surfaceDark = Color(0xFF19120C)
+val onSurfaceDark = Color(0xFFFFFAF8)
+val onSurfaceVariantDark = Color(0xFFD9C8B9)
+
+val surfaceContainerLowestDark = Color(0xFF130D07)
+val surfaceContainerLowDark = Color(0xFF211A14)
+val surfaceContainerHighDark = Color(0xFF302921)
+val surfaceContainerHighestDark = Color(0xFF3B332C)
+
+val outlineDark = Color(0xFFB0A092)
+val outlineVariantDark = Color(0xFF8F8174)
+
+val inversePrimaryDark = Color(0xFF693E00)
+val scrimDark = Color(0xFF000000)
