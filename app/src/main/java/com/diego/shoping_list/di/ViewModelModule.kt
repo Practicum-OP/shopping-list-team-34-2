e46322565c62f0c8de38e523a081ed4b70dd94a9
Diego <138886655+Egoroof1@users.noreply.github.com>
2026-09-23 00:05:02 +0300

    Update ViewModelModule.kt

--- app/src/main/java/com/diego/shoping_list/di/ViewModelModule.kt
+++ app/src/main/java/com/diego/shoping_list/di/ViewModelModule.kt
@@ -1,7 +1,6 @@
-
-import com.diego.shoping_list.presentation.products.ProductsViewModel
 package com.diego.shoping_list.di
 
+import com.diego.shoping_list.presentation.products.ProductsViewModel
 import com.diego.shoping_list.presentation.shoppingList.view_model.ShoppingListViewModel
 import com.diego.shoping_list.presentation.start.StartScreenViewModel
 import org.koin.core.module.dsl.viewModel
